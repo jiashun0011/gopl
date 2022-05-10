95c8579e2b2fc8d5d64b1f1d66bf930f9da9471c
jiashun <573255300@qq.com>
2022-05-10 07:40:38 +0000

    code for ch3

--- ch2/tempconv/tempconv.go
+++ ch2/tempconv/tempconv.go
@@ -6,16 +6,15 @@ type Celsius float64
 type Fahrenheit float64
 
 const (
-    AbsoluteZeroC Celsius = -273.15
-    FreezingC     Celsius = 0
-    BoilingC      Celsius = 0
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 0
 )
 
-
 func (c Celsius) String() string {
-    return fmt.Sprintf("%gC", c)
+	return fmt.Sprintf("%gC", c)
 }
 
 func (f Fahrenheit) String() string {
-    return fmt.Sprintf("%gF", f)
+	return fmt.Sprintf("%gF", f)
 }
